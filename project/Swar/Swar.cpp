ebca024091c7850fa85d11e4c4d0f4bfef4557f7
sally <sally4388you@126.com>
2018-03-24 20:19:10 -0700

    little success for a second

--- project/Swar/Swar.cpp
+++ project/Swar/Swar.cpp
@@ -5,9 +5,11 @@
 ** 4) How to test performance?
 **
 ** 5) Constants are all changed to i128? (Or changed to i32??)
-** 6) BinaryOperator::createAnd??
-** 7) Vectors are changed to i18 or i128?
+** 6) Mask 149796 -> -112348???????
+** 7) Vectors are changed to i18 or i128? zext or bitcast??
 ** 8) How to move mask generator outside the function? (I can optimize it tho...)
+**
+** ./bin/opt -load lib/LLVMSwar.dylib -Swar /Users/sally/code/test/add2.ll
 */
 
 #include "llvm/Pass.h"
@@ -20,18 +22,21 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
 #include <stdlib.h>
 
 using namespace llvm;
+
+#define HIGHMASK false
+#define LOWMASK true
 
 namespace {
     struct Swar : public FunctionPass {
         static char ID;
         Swar() : FunctionPass(ID) {}
 
-        uint64_t generateHMask(VectorType *);
-        uint64_t generateLMask(VectorType *);
-        Value * swar_add(Value * a1, Value * a2, uint64_t lmask, uint64_t hmask);
+        Constant * generateMask(VectorType * vectorType, bool isLow);
+        void swar_add(Value * a1, Value * a2, Value * lmask, Value * hmask, Instruction * InsertBefore=nullptr);
 
         bool runOnFunction(Function &function) override {
             errs() << function.getName() << ":\n";
@@ -39,13 +44,15 @@ namespace {
             bool modified = false;
 
             for (auto &block : function) {
-                // std::vector<BinaryOperator *> bitCastInstsToErase;
+                std::vector<BinaryOperator *> addInstsToErase;
 
                 for (auto &inst : block) {
 
                     if (!inst.isBinaryOp() || inst.getOpcode() != Instruction::Add) {
                         continue;
                     }
+                    BinaryOperator * addInst = dyn_cast<BinaryOperator>(&inst);
+                    LLVMContext & context = llvm::getGlobalContext();
                     errs() << "  found binary instruction\n";
 
                     Value * vector1 = inst.getOperand(0);
@@ -54,30 +61,38 @@ namespace {
                     auto vectorType = dyn_cast<VectorType>(vector1->getType());
                     auto elemSize = vectorType->getScalarSizeInBits();
                     auto numElems = vectorType->getNumElements();
-                    errs() << "    from vector <" << numElems << " x i" << elemSize
-                          << ">\n";
+                    errs() << "    from vector <" << numElems << " x i" << elemSize << ">\n";
 
                     auto vectorSize = elemSize * numElems;
-                    Type * IntTy;
+                    Type * castTy;
                     if (vectorSize > 256) { // larger than 256
                         errs() << "Length larger than 256\n";
                         continue;
-                    } else if (vectorSize > 128) { // larger than 128, smaller than 256
-                        // IntTy = llvm::IntegerType::getInt256Ty(inst.getContext());
-                        errs() << "Doesn't find 256 yet\n";
-                        continue;
-                    } else { // smaller than 128
-                        IntTy = llvm::IntegerType::getInt128Ty(inst.getContext());
+                    } else {
+                        castTy = llvm::IntegerType::get(context, vectorSize);
                     }
                     modified = true;
 
                     // ???? Where is the destination?
-                    BitCastInst * BCI1 = new llvm::BitCastInst(vector1, IntTy, "", &inst);
-                    BitCastInst * BCI2 = new llvm::BitCastInst(vector2, IntTy, "", &inst);
-                    uint64_t lmask = generateLMask(vectorType);
-                    uint64_t hmask = generateHMask(vectorType);
-                    Value * resultInt = swar_add(vector1, vector2, lmask, hmask);
-                    BitCastInst * BCI3 = new llvm::BitCastInst(resultInt, vectorType, "", &inst);
+                    BitCastInst * a1 = new BitCastInst(vector1, castTy, "", addInst);
+                    BitCastInst * a2 = new BitCastInst(vector2, castTy, "", addInst);
+                    Constant * lmask = generateMask(vectorType, LOWMASK);
+                    Constant * hmask = generateMask(vectorType, HIGHMASK);
+
+                    // // Value * resultInt = swar_add((Value *)BCI1, (Value *)BCI2, _lmask, _hmask, addInst);
+                    BinaryOperator * m1 = BinaryOperator::Create(Instruction::And, (Value *)a1, (Value *)lmask, "", addInst);
+                    BinaryOperator * m2 = BinaryOperator::Create(Instruction::And, (Value *)a2, (Value *)lmask, "", addInst);
+                    BinaryOperator * r1 = BinaryOperator::Create(Instruction::Add, (Value *)m1, (Value *)m2, "", addInst);
+                    BinaryOperator * r2 = BinaryOperator::Create(Instruction::Xor, (Value *)a1, (Value *)a2, "", addInst);
+                    BinaryOperator * r3 = BinaryOperator::Create(Instruction::And, (Value *)r2, (Value *)hmask, "", addInst);
+                    BinaryOperator * r4 = BinaryOperator::Create(Instruction::Xor, (Value *)r1, (Value *)r3, "", addInst);
+
+                    BitCastInst * BCI3 = new llvm::BitCastInst((Value *)r4, vectorType, "", addInst);
+                    addInstsToErase.push_back(addInst);
+                }
+
+                for (auto &instr : addInstsToErase) {
+                    instr->eraseFromParent();
                 }
             }
 
@@ -90,25 +105,24 @@ char Swar::ID = 0;
 
 static RegisterPass<Swar> X("Swar", "SWAR", false, false);
 
-Value * swar_add(Value * a1, Value * a2, uint64_t lmask, uint64_t hmask) {
-    IRBuilder<> builder(llvm::getGlobalContext());
+void swar_add(Value * a1, Value * a2, Value * lmask, Value * hmask, Instruction *InsertBefore=nullptr) {
+    // IRBuilder<> builder(llvm::getGlobalContext());
 
-    Value * m1 = builder.CreateAnd(a1, lmask);
-    Value * m2 = builder.CreateAnd(a2, lmask);
-    Value * r1 = builder.CreateAdd(m1, m2);
-    Value * r2 = builder.CreateXor(a1, a2);
-    Value * r3 = builder.CreateAnd(r2, hmask);
-    Value * r4 = builder.CreateXor(r1, r3);
-    // BinaryOperator *m1 = new BinaryOperator(Instruction::And, vector1, mask, "", tail);
-    // BinaryOperator *m2 = new BinaryOperator(Instruction::And, vector2, mask, "", tail);
-
-    return r4;
+    // BinaryOperator::Create(Instruction::And, a1, lmask, "");
+    // BinaryOperator::Create(Instruction::And, a2, lmask, "");
+    // Value * m1 = builder.CreateAnd(a1, lmask);
+    // Value * m2 = builder.CreateAnd(a2, lmask);
+    // Value * r1 = builder.CreateAdd(m1, m2);
+    // Value * r2 = builder.CreateXor(a1, a2);
+    // Value * r3 = builder.CreateAnd(r2, hmask);
+    // Value * r4 = builder.CreateXor(r1, r3);
 }
 
 // Need to OPTIMIZE!!
-uint64_t Swar::generateLMask(VectorType * vectorType) {
+Constant * Swar::generateMask(VectorType * vectorType, bool isLow = false) {
     auto elemSize = vectorType->getScalarSizeInBits();
     auto numElems = vectorType->getNumElements();
+    Type * castTy = llvm::IntegerType::get(llvm::getGlobalContext(), elemSize * numElems);
 
     char * ptr;
     char string[128];
@@ -117,10 +131,10 @@ uint64_t Swar::generateLMask(VectorType * vectorType) {
     unsigned int j;
     for (j = 0; j < elemSize; j ++) {
         if (j == 0) {
-            unit[j] = '0';
+            unit[j] = (isLow) ? '0' : '1';
             continue;
         }
-        unit[j] = '1';
+        unit[j] = (isLow) ? '1' : '0';
     }
     unit[j] = '\0';
 
@@ -131,35 +145,8 @@ uint64_t Swar::generateLMask(VectorType * vectorType) {
     }
     string[j * elemSize] = '\0';
 
-    long int parsed = strtol(string, &ptr, 2);
-    return parsed;
-}
+    unsigned long int parsed = strtol(string, &ptr, 2);
+    Constant * mask = llvm::ConstantInt::get(castTy, parsed);
 
-uint64_t Swar::generateHMask(VectorType * vectorType) {
-    auto elemSize = vectorType->getScalarSizeInBits();
-    auto numElems = vectorType->getNumElements();
-
-    char * ptr;
-    char string[128];
-    char unit[128];
-
-    unsigned int j;
-    for (j = 0; j < elemSize; j ++) {
-        if (j == 0) {
-            unit[j] = '1';
-            continue;
-        }
-        unit[j] = '0';
-    }
-    unit[j] = '\0';
-
-    for (j = 0; j < numElems; j ++) {
-        for (unsigned int i = 0; i < elemSize; i ++) {
-            string[j * elemSize + i] = unit[i];
-        }
-    }
-    string[j * elemSize] = '\0';
-
-    long int parsed = strtol(string, &ptr, 2);
-    return parsed;
+    return mask;
 }
