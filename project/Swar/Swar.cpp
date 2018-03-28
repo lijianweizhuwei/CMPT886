63c236de8eccbd5ef4ccb6e95f13093ea022f2cf
sally <sally4388you@126.com>
2018-03-28 08:56:29 -0700

    swar update 2

--- project/Swar/Swar.cpp
+++ project/Swar/Swar.cpp
@@ -1,15 +1,15 @@
 /* Problems I have now:
 ** 1) We need to use BasicBlockPass instead of FunctionPass (difference?)
-** 2) How to remove the original instructions?
-** 3) How to test the result?
-** 4) How to test performance?
 **
 ** 5) Constants are all changed to i128? (Or changed to i32??)
-** 6) Mask 149796 -> -112348???????
-** 7) Vectors are changed to i18 or i128? zext or bitcast??
-** 8) How to move mask generator outside the function? (I can optimize it tho...)
+** 6) Mask 149796 -> -112348??????? two-complement something.
 **
 ** ./bin/opt -load lib/LLVMSwar.dylib -Swar /Users/sally/code/test/add2.ll
+** updaet2
+** 1) test performance
+** 2) bitcast
+** 3) negative numbers
+**
 */
 
 #include "llvm/Pass.h"
@@ -36,7 +36,8 @@ namespace {
         Swar() : FunctionPass(ID) {}
 
         Constant * generateMask(VectorType * vectorType, bool isLow);
-        void swar_add(Value * a1, Value * a2, Value * lmask, Value * hmask, Instruction * InsertBefore=nullptr);
+        Value * swar_add(Value * a1, Value * a2, VectorType * vectorType, Instruction * addInst);
+        Value * bitcast(Value * vector, BinaryOperator * addInst);
 
         bool runOnFunction(Function &function) override {
             errs() << function.getName() << ":\n";
@@ -73,22 +74,15 @@ namespace {
                     }
                     modified = true;
 
-                    // ???? Where is the destination?
-                    BitCastInst * a1 = new BitCastInst(vector1, castTy, "", addInst);
-                    BitCastInst * a2 = new BitCastInst(vector2, castTy, "", addInst);
-                    Constant * lmask = generateMask(vectorType, LOWMASK);
-                    Constant * hmask = generateMask(vectorType, HIGHMASK);
+                    Value * a1 = bitcast(vector1, addInst);
+                    Value * a2 = bitcast(vector2, addInst);
 
-                    // // Value * resultInt = swar_add((Value *)BCI1, (Value *)BCI2, _lmask, _hmask, addInst);
-                    BinaryOperator * m1 = BinaryOperator::Create(Instruction::And, (Value *)a1, (Value *)lmask, "", addInst);
-                    BinaryOperator * m2 = BinaryOperator::Create(Instruction::And, (Value *)a2, (Value *)lmask, "", addInst);
-                    BinaryOperator * r1 = BinaryOperator::Create(Instruction::Add, (Value *)m1, (Value *)m2, "", addInst);
-                    BinaryOperator * r2 = BinaryOperator::Create(Instruction::Xor, (Value *)a1, (Value *)a2, "", addInst);
-                    BinaryOperator * r3 = BinaryOperator::Create(Instruction::And, (Value *)r2, (Value *)hmask, "", addInst);
-                    BinaryOperator * r4 = BinaryOperator::Create(Instruction::Xor, (Value *)r1, (Value *)r3, "", addInst);
+                    Value * r4 = swar_add(a1, a2, vectorType, addInst);
 
-                    BitCastInst * BCI3 = new llvm::BitCastInst((Value *)r4, vectorType, "", addInst);
+                    BitCastInst * BCI3 = new llvm::BitCastInst(r4, vectorType, "", addInst);
+
                     addInstsToErase.push_back(addInst);
+                    addInst->replaceAllUsesWith((Value *)BCI3);
                 }
 
                 for (auto &instr : addInstsToErase) {
@@ -105,17 +99,61 @@ char Swar::ID = 0;
 
 static RegisterPass<Swar> X("Swar", "SWAR", false, false);
 
-void swar_add(Value * a1, Value * a2, Value * lmask, Value * hmask, Instruction *InsertBefore=nullptr) {
-    // IRBuilder<> builder(llvm::getGlobalContext());
+/**
+ * Lists all Check models.
+ * @return mixed
+ */
+Value * Swar::swar_add(Value * a1, Value * a2, VectorType * vectorType, Instruction * addInst) {
+    Constant * lmask = generateMask(vectorType, LOWMASK);
+    Constant * hmask = generateMask(vectorType, HIGHMASK);
 
-    // BinaryOperator::Create(Instruction::And, a1, lmask, "");
-    // BinaryOperator::Create(Instruction::And, a2, lmask, "");
-    // Value * m1 = builder.CreateAnd(a1, lmask);
-    // Value * m2 = builder.CreateAnd(a2, lmask);
-    // Value * r1 = builder.CreateAdd(m1, m2);
-    // Value * r2 = builder.CreateXor(a1, a2);
-    // Value * r3 = builder.CreateAnd(r2, hmask);
-    // Value * r4 = builder.CreateXor(r1, r3);
+    BinaryOperator * m1 = BinaryOperator::Create(Instruction::And, (Value *)a1, (Value *)lmask, "", addInst);
+    BinaryOperator * m2 = BinaryOperator::Create(Instruction::And, (Value *)a2, (Value *)lmask, "", addInst);
+    BinaryOperator * r1 = BinaryOperator::Create(Instruction::Add, (Value *)m1, (Value *)m2, "", addInst);
+    BinaryOperator * r2 = BinaryOperator::Create(Instruction::Xor, (Value *)a1, (Value *)a2, "", addInst);
+    BinaryOperator * r3 = BinaryOperator::Create(Instruction::And, (Value *)r2, (Value *)hmask, "", addInst);
+    BinaryOperator * r4 = BinaryOperator::Create(Instruction::Xor, (Value *)r1, (Value *)r3, "", addInst);
+
+    return (Value *)r4;
+}
+
+/**
+ * Bitcast Legalization
+ * Ex: <6 * i3> <i3 3, i3 4, i3 2, i3 1, i3 0, i3 1>
+ * To: <i18> Octal: 101243 Decimal: 33443
+ * @return mixed
+ */
+Value * Swar::bitcast(Value * vector, BinaryOperator * addInst) {
+    auto vectorType = cast<VectorType>(vector->getType());
+    auto elemSize = vectorType->getScalarSizeInBits();
+    auto numElems = vectorType->getNumElements();
+    auto totalSize = elemSize * numElems;
+
+    // assert(numElems > 1 && "BitcastLegalization has nothing to do with v1!");
+
+    unsigned int legalizedTotalSize = 8;
+    while (legalizedTotalSize < totalSize) {
+        legalizedTotalSize *= 2;
+    }
+
+    IRBuilder<> builder(addInst);
+
+    Value * lastValue;
+    for (unsigned int i = 0; i < numElems; ++i) {
+        Value * elemValue = builder.CreateExtractElement(vector, i);
+        Value * zextElemValue = builder.CreateZExt(elemValue, IntegerType::get(builder.getContext(), legalizedTotalSize));
+
+        if (i == 0) {
+            lastValue = zextElemValue;
+        }
+        else {
+            Value * shiftedValue = builder.CreateShl(zextElemValue, i * elemSize);
+            lastValue = builder.CreateOr(lastValue, shiftedValue);
+        }
+    }
+
+    Value * result = builder.CreateTrunc(lastValue, IntegerType::get(builder.getContext(), totalSize));
+    return result;
 }
 
 // Need to OPTIMIZE!!
--- project/update2_swar.md
+++ project/update2_swar.md
@@ -18,32 +18,28 @@ define i32 @main() {
 ```llvm
 define i32 @main() {
   ; After optimizatioin
-  %v1 = bitcast <6 x i3> <i3 3, i3 4, i3 2, i3 1, i3 0, i3 1> to i18
-  %v2 = bitcast <6 x i3> <i3 4, i3 4, i3 4, i3 4, i3 1, i3 2> to i18
+  ; %v1 = <6 x i3> <i3 3, i3 4, i3 2, i3 1, i3 0, i3 1>
+  ; %v2 = <6 x i3> <i3 4, i3 4, i3 4, i3 4, i3 1, i3 2>
 
-  %low_v1 = and i18 %v1, 112347 ;get low bits for every field
-  %low_v2 = and i18 %v2, 112347
+  %low_v1 = and i18 33443, 112347 ;get low bits for every field
+  %low_v2 = and i18 71972, 112347
   ;decimal 112347 = binary 011 011 011 011 011 011
   ;i18 %v1:     < 3   4   2   1   0   1 >
   ;AND: %v1     <011 100 010 001 000 001> & 
   ;     112347  <011 011 011 011 011 011>
   ;i18 %low_v1: <011 000 010 001 000 001>
   ;i18 %low_v2: <000 000 000 000 001 010>
-
   %low_add = add i18 %low_v1, %low_v2 ; get the result of addition of two vectors' low bits.
   ;i18 %low_add: <011 000 010 001 001 011>
-
-  %overflow = xor i18 %v1, %v2
+  %overflow = xor i18 33443, 71972
   ;i18 %v1: <011 100 010 001 000 001>
   ;i18 %v2: <100 100 100 100 001 010>
   ;XOR i18: <111 000 110 101 001 011>
-
   %high_add = and i18 %overflow, 149796
   ;decimal 149796 = binary 100 100 100 100 100 100
   ;AND: %overflow: <111 000 110 101 001 011> &
   ;        149796: <100 100 100 100 100 100>
   ;i18  %high_add: <100 000 100 100 000 000>
-
   %result = xor i18 %low_add, %high_add
   ;XOR:  %low_add: <011 000 010 001 001 011> or
   ;     %high_add: <100 000 100 100 000 000>
@@ -57,7 +53,7 @@ define i32 @main() {
 
 #### Subtraction Algorithm
 ```
-SWAR sub z = x - y
+SWAR sub z = x - y;
 
 z = ((x | H) - (y &~H)) ^ ((x ^~y) & H)
 ```
@@ -73,35 +69,30 @@ define i32 @main() {
 ```llvm
 define i32 @main() {
   ; After optimizatioin
-  %v1 = bitcast <6 x i3> <i3 4, i3 4, i3 4, i3 4, i3 1, i3 2> to i18
-  %v2 = bitcast <6 x i3> <i3 3, i3 4, i3 2, i3 1, i3 0, i3 1> to i18
+  ; %v1 = <6 x i3> <i3 4, i3 4, i3 4, i3 4, i3 1, i3 2>
+  ; %v2 = <6 x i3> <i3 3, i3 4, i3 2, i3 1, i3 0, i3 1>
 
-  %low_v1 = or i18 %v1, 149796 ;get low bits for every field
+  %low_v1 = or i18 71972, 149796 ;get low bits for every field
   ;i18 %v1:     < 4   4   4   4   1   2 >
   ;OR: %v1      <100 100 100 100 001 010> & 
   ;     149796  <100 100 100 100 100 100>
   ;i18 %low_v1: <100 100 100 100 101 110>
-
-  %low_v2 = and i18 %v2, 112347
+  %low_v2 = and i18 33443, 112347
   ;i18 %v2:     < 3   4   2   1   0   1 >
   ;AND: %v2     <011 100 010 001 000 001> & 
   ;     112347  <011 011 011 011 011 011>
   ;i18 %low_v2: <011 000 010 001 000 001>
-
   %low_sub = sub i18 %low_v1, %low_v2 ; get the result of addition of two vectors' low bits.
   ;i18 %low_sub: <001 100 010 011 101 101>
-
-  %_v2 = xor i18 %v2, 262143
-  %overflow = xor i18 %v1, %_v2
+  %_v2 = xor i18 33443, 262143
+  %overflow = xor i18 71972, %_v2
   ;i18 %v1:  <100 100 100 100 001 010>
   ;i18 %_v2: <100 011 101 110 111 110>
   ;XOR i18:  <000 111 001 010 110 100>
-
   %high_sub = and i18 %overflow, 149796
   ;AND: %overflow: <000 111 001 010 110 100> &
   ;        149796: <100 100 100 100 100 100>
   ;i18  %high_sub: <000 100 000 000 100 100>
-  
   %result = xor i18 %low_sub, %high_sub
   ;XOR:  %low_sub: <001 100 010 011 101 101> or
   ;     %high_sub: <000 100 000 000 100 100>
@@ -112,7 +103,11 @@ define i32 @main() {
   ret i32 0
 }
 ```
-
+#### Average Algorithm
+```
+SWAR average z = (x+y)/2 based on x + y = (x^y) + 2*(x&y)
+    z = (x & y) + (((x ^ y) & ~L) >> 1)
+```
 #### Accomplishment
 Original file: test.ll
 ```llvm
@@ -136,11 +131,39 @@ After pass:
 
 Running time:
 ![Alt text](../image//swar_update22.jpg)
-
 #### Difficulties
-* bitcast
-* negative numbers
+* Bitcast
+```llvm
+  %b1 = trunc i18 %result to i3
+  %shr1 = lshr i18 %result, 3
+  %b2 = trunc i18 %shr1 to i3
+  %shr2 = lshr i18 %b2, 3
+  %b3 = trunc i18 %shr2 to i3
+  %shr3 = lshr i18 %b3, 3
+  %b4 = trunc i18 %shr3 to i3
+  %shr4 = lshr i18 %b4, 3
+  %b5 = trunc i18 %shr4 to i3
+  %shr5 = lshr i18 %b5, 3
+  %b6 = trunc i18 %shr5 to i3
 
+  %c = alloca <6 x i3>
+  %c1 = load <6 x i3>, <6 x i3>* %c
+  %c2 = insertelement <6 x i3> %c1, i3 %b0, i8 0
+  %c3 = insertelement <6 x i3> %c2, i3 %b1, i8 1
+  %c4 = insertelement <6 x i3> %c3, i3 %b2, i8 2
+  %c5 = insertelement <6 x i3> %c4, i3 %b3, i8 3
+  %c6 = insertelement <6 x i3> %c5, i3 %b4, i8 4
+  %c7 = insertelement <6 x i3> %c6, i3 %b5, i8 5
+```
+* Negative numbers - Two-complements'
 <6 x i3> <-1, 0, -2, -3, -1, -1>
-
 <6 x i3> <7, 0, 6, 5, 7, 7>
+
+#### Next
+* Optimization
+* Other Operations
+  * Multiplicatioin/Division
+  * Bit Reverse
+  * Log2 of an Integer
+  * Most Significant 1 Bit
+  * Popcount
